ea1d1b993ef1844af72baa139aa80f725c0ca2ea
Mikael Hargeus <Mikael.Hargeus@live.se>
2022-11-20 01:18:23 +0100

    finished endpoints

---
+++ CoCalcBackEnd/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoCalcBackEnd.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
